2c7400dfea2ea95bfe6783b5444a65f535a08324
Claudio Cannizzaro <claudio.cannizzaro@fgcz.uzh.ch>
2025-06-05 09:33:59 +0200

    Add initial project structure; create pyproject.toml and launcher script

---
+++ launcher.py
@@ -0,0 +1,9 @@
+def main():
+    print("BioBeamerLauncher: Placeholder script.")
+    print(
+        "This script will fetch the config, manage BioBeamer versions, and launch BioBeamer."
+    )
+
+
+if __name__ == "__main__":
+    main()
