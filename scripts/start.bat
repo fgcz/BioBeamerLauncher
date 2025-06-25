fd48b86aaf05b43e1646b29d4bce203fbfa1e1a7
Claudio Cannizzaro <claudio.cannizzaro@fgcz.uzh.ch>
2025-06-25 16:34:58 +0200

    bulk commit

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,4 +1,8 @@
 @echo off
+
+REM Always run from project root (parent of this script's directory) - same as setup.bat
+cd /d "%~dp0\.."
+
 call biobeamer-launcher-venv\Scripts\activate.bat
 biobeamer-launcher --config config\launcher.ini
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -e
+
+# Always run from project root (parent of this script's directory) - same as setup.sh
+cd "$(dirname "$0")/.."
+
 source biobeamer-launcher-venv/bin/activate
 biobeamer-launcher --config config/launcher.ini
 
